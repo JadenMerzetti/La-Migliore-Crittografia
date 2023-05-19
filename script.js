8640b60f6812e29f6cec5d79bb61df77a863cf9a
jaden merzetti <jmerzet1@unb.ca>
2023-05-19 02:32:36 -0300

    Fixed the loop inconsitency and the proclivity for the loop to produce 10 as a character which would be 2 characters in length

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ while (true) {
   const CharacterAmount = parseInt(userCharacterAmount);
   
   if (CharacterAmount >= 8 && CharacterAmount <= 129 ) {
-    console.log("User wants password to be" + userCharacterAmount + " characters in length!")
       break;
 
   }
@@ -21,41 +20,55 @@ const userNumbersAlert           = window.confirm("Please agree to include numbe
 const userLowerCaseAlert         = window.confirm("Please agree to lower-case letters in your password!");
 const userUpperCaseAlert         = window.confirm("Please agree to upper-case letters in your password!");
 
-
+let passwordArray;
 function generatePassword () {
 
 passwordArray = [];
 var i  = 0;
 
-while (i < userCharacterAmount) {
+while (i < userCharacterAmount ) {
   const categoryInt = Math.floor(Math.random() * 4);
 
   if (categoryInt === 0) {
     var lowerCaseCode = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
     var randomLowerCaseLetter = String.fromCharCode(lowerCaseCode);
     passwordArray.push(randomLowerCaseLetter);
+    i++;
+  
   }
   else if (categoryInt === 1) {
     var upperCaseCode = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
     var randomUpperCaseLetter = String.fromCharCode(upperCaseCode);
     passwordArray.push(randomUpperCaseLetter);
+    i++;
+ 
   }
   else if (categoryInt === 2) {
-    var number = Math.floor(Math.random() * 10);
+    var number = Math.floor(Math.random() * 9);
     passwordArray.push(number);
+    i++;
+  
   }
-  else (categoryInt === 3); {
+  else if (categoryInt === 3) {
     var symbol = Math.floor(Math.random() * (41 - 33 + 1)) + 33;
     var randomSymbolCode = String.fromCharCode(symbol);
     passwordArray.push(randomSymbolCode);
+    i++;
+    
   }
 
-  i++;
+
 }
 
+console.log(passwordArray.length)
 return passwordArray.join('');
 
 }
+console.log(userCharacterAmount)
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -68,6 +81,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// I created a generator to randomly pick between 1 and 4. This would act like a 
+// category choice of which type of character (Number, Uppercase, Lowercase, or symbol) would be added to the array next
+// then my loop randomly chooses a number within the range of the character codes within said category
+// it would then convert that number into the character itself and procees to add it to the array 
+// that way each loop will generate a random character from a random category making the password ULTRA RANDOM
 
 
 
